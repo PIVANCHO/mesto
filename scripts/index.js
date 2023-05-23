848cc935362ebfe3f029a91a9cd11c8e00de150e
Ivan Pavlenko <osnobox163rus@ya.ru>
2023-05-23 22:17:31 +0400

    fixed const name, transition, images alt and opacity

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const buttonEdit = document.querySelector('.profile__edit-button');
-const buttonClose = document.querySelectorAll('.popup__close-button');
+const buttonsClose = document.querySelectorAll('.popup__close-button');
 const buttonAdd = document.querySelector('.profile__add-button');
 const popupEdit = document.querySelector('#popup_edit');
 const popupAdd = document.querySelector('#popup_add');
@@ -14,7 +14,8 @@ const popupFormAdd = document.querySelector('#popup__form_add');
 const imagePopup = document.querySelector('#big-image');
 const cardsContainer = document.querySelector('.elements');
 const cardTemplate = document.querySelector('.card-template').content;
-
+const imagePopupPicture = imagePopup.querySelector('.popup__image');
+const imagePopupSignature = imagePopup.querySelector('.popup__signature');
 
 function openPopup(popup) {             //Open any popup
   popup.classList.add('popup_opened');
@@ -28,15 +29,15 @@ function createCard(cardData) {        //Creating a card
   const itemElement = cardTemplate.cloneNode(true);
   const itemElementSignature = itemElement.querySelector('.element__signature');
   const itemElementImage = itemElement.querySelector('.element__image');
-  const imagePopupPicture = imagePopup.querySelector('.popup__image');
-  const imagePopupSignature = imagePopup.querySelector('.popup__signature');
   const cardLikeButton = itemElement.querySelector('.element__like');
   const cardDeleteButton = itemElement.querySelector('.element__delete');
   itemElementSignature.textContent = cardData.name;
   itemElementImage.src = cardData.link;
+  itemElementImage.alt = cardData.name;
   itemElementImage.addEventListener('click', (evt) => {
     openPopup(imagePopup);
     imagePopupPicture.src = cardData.link;
+    imagePopupPicture.alt = cardData.name;
     imagePopupSignature.textContent = cardData.name;
   });
   cardLikeButton.addEventListener('click', (evt) => {
@@ -53,7 +54,7 @@ function addCard(card) {                //Adding a card to container
   cardsContainer.prepend(card);
 }
 
-buttonClose.forEach((item) => {            // Close opened popup by button
+buttonsClose.forEach((item) => {            // Close opened popup by button
   item.addEventListener('click', (evt) => {
     closePopup(evt.target.closest('.popup'));
   });
@@ -78,7 +79,7 @@ buttonAdd.addEventListener('click', () => {    //Open card adding popup
 
 popupFormAdd.addEventListener('submit', (evt) => {   //Add form submititon
   evt.preventDefault();
-  addCard(createCard({name: cardNameInput.value, link: imageLinkInput.value}));
+  addCard(createCard({ name: cardNameInput.value, link: imageLinkInput.value}));
   popupFormAdd.reset();
   closePopup(popupAdd);
 });
